e18d8526cf2fa1701ae3c86c1738ef36567c58d4
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-10-05 11:59:04 -0500

    Add env variables to .travis.yml

--- config/database.php
+++ config/database.php
@@ -69,8 +69,8 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
